723b8be5d569a2283019a8657fd41c8a3bf7ac70
sttagami2 <sttagami2@gmail.com>
2020-04-25 15:42:23 +0900

    lesson8

--- main.js
+++ main.js
@@ -19,10 +19,30 @@
       const main = document.querySelector('main');
       main.appendChild(section);
     }
+
+    getRandomImage() {
+      const images = [
+        'img/sissors.png',
+        'img/block.png',
+        'img/paper.png',
+      ];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin() {
+      this.img.src = this.getRandomImage();
+    }
   }
 
   const panels = [
     new Panel(),
     new Panel(),
   ];
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    panels.forEach(panel => {
+      panel.spin();
+    });
+  });
 }
